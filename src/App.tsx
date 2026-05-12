bed36e7819a433b99312c2da43314713a4c7228e
Tsekpoe Blessing Eyram <betsekpoe@st.knust.edu.gh>
2026-05-12 14:47:24 +0000

    Refactor: Wrap with ProjectProvider to use context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { RouterProvider } from "react-router-dom"
+import { ProjectProvider } from "./context/ProjectContext"
 
 import { router } from "./routes"
 
 
 export default function App() {
 	return <div className="flex justify-around m-20 mx-0">
-		<RouterProvider router={router} />
+		<ProjectProvider>
+			<RouterProvider router={router} />
+		</ProjectProvider>
 	</div>
 }
